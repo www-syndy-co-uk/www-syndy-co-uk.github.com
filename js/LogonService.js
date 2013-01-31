bb38b000a684321d7644dc16280a67d19364829b
www-syndy-co-uk <dev@syndy.co.uk>
2013-01-31 17:08:38 +0000

    Use separate login and logout URLs

--- js/LogonService.js
+++ js/LogonService.js
@@ -6,10 +6,12 @@ function LogonService (apiRoot) {
     this.apiRoot = apiRoot;
 }
 
-LogonService.prototype.checkLoggedInStatus = function (destUrl) {
-    destUrl = destUrl || "/";
-    var internalRedirectUrl = "/authentication/redirectToBlog?destUrl=" + destUrl;
-    var url = this.apiRoot + "/authentication/loginInfo?destUrl=" + encodeURIComponent(internalRedirectUrl);
+LogonService.prototype.checkLoggedInStatus = function (postLoginUrl, postLogoutUrl) {
+    postLoginUrl = postLoginUrl || "/";
+    postLogoutUrl = postLogoutUrl || "/";
+    var postLoginUrl = "/authentication/redirectToBlog?destUrl=" + postLoginUrl;
+    var postLogoutUrl = "/authentication/redirectToBlog?destUrl=" + postLogoutUrl;
+    var url = this.apiRoot + "/authentication/loginInfo?postLoginUrl=" + encodeURIComponent(postLoginUrl) + "&postLogoutUrl=" + encodeURIComponent(postLogoutUrl);
     url += "&callback=?";
     return $.getJSON(url);
 };
