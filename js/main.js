abac0c1870fd538ad08008f4d8b3c3d286c5e527
www-syndy-co-uk <dev@syndy.co.uk>
2013-02-10 05:17:22 +0000

    Minor JS formatting

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-window.writelnScript = function (src) {
+window.writelnScript = function(src) {
     document.writeln("<script src='" + src + "'></scr" + "ipt>");
 };
 
-window.writelnCSS = function (href) {
+window.writelnCSS = function(href) {
     document.writeln("<link rel=stylesheet type=text/css href='" + href + "'>");
 };
