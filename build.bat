2a669118eb7b1d7348126c685d93d72283bf040e
www-syndy-co-uk <dev@syndy.co.uk>
2014-02-04 18:30:38 +0000

    Script to build the source using Node/r.js.

---
+++ build.bat
@@ -0,0 +1,15 @@
+@echo off
+
+set r_js_shared_opts=-o baseUrl=js name=main paths.jquery=empty: paths.underscore=empty:
+
+call r.js.cmd %r_js_shared_opts% out=temp.js optimize=none 
+
+rem copy /b js\blogger\head.js+temp.js built.js
+copy /b temp.js built.js
+
+call r.js.cmd %r_js_shared_opts% out=temp.js
+
+rem copy /b js\blogger\head.js+temp.js built.min.js
+copy /b temp.js built.min.js
+
+del temp.js
