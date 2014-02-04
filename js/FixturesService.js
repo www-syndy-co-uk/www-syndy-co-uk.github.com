55fd9e3c0745624e65c36b0e1020df35185f0d19
www-syndy-co-uk <dev@syndy.co.uk>
2014-02-04 16:51:00 +0000

    Now handles slim and regular fixtures JSON.

--- js/FixturesService.js
+++ js/FixturesService.js
@@ -1,21 +1,5 @@
 /*global window, alert*/
-define(["jquery"], function (jQuery) {
-
-var $ = jQuery;
-
-function FixturesService(host, port) {
-    var me = this;
-
-    host = host || "//api.syndy.co.uk";
-    port = port || "";
-    if (port) {
-        host = host + ":" + port;
-    }
-
-    this.data = null;
-    this.fixtures = null;
-    this.rounds = null;
-    this.teams = null;
+define(["jquery"], function ($) {
 
     function getRoundsFromFixtures(fixtures) {
         var rounds = [];
@@ -35,23 +19,83 @@ function FixturesService(host, port) {
         return rounds;
     }
 
+    function resolveTeam(team, teams) {
+        if (!teams) {
+            return team;
+        }
+        return teams[team];
+    }
+
+    /**
+     * Create a map of [team name/string] -> [team id/int].
+     */
+    function createTeamIds(fixtures, teams) {
+        // Keep a team id Map for adding id attrs to team elements
+        var teamIds = {};
+        var teamIdCount = 0;
+        _.each(fixtures, function(fixture, fixtureIdx) {
+            var team1 = resolveTeam(fixture.teamId1, teams);
+            var team2 = resolveTeam(fixture.teamId2, teams);
+            // ids are used for clicks
+            var teamId1 = teamIds[team1];
+            if ("undefined" === typeof teamId1) {
+                teamId1 = teamIds[team1] = teamIdCount++;
+            }
+            var teamId2 = teamIds[team2];
+            if ("undefined" === typeof teamId2) {
+                teamId2 = teamIds[team2] = teamIdCount++;
+            }
+        });
+        console.log(teamIds);
+        return teamIds;
+    }
+
+    function handleData(data) {
+        var d = {};
+        // slim fixtures (smaller json) have data.fixtures. regular fixtures do not.
+        var fixtures = data.fixtures ? data.fixtures : data;
+        d.rounds = getRoundsFromFixtures(fixtures);
+        d.fixtures = fixtures;
+        d.teams = data.teams || [];
+        return d;
+    }
+
+    function FixturesService(opts) {
+        opts = opts || {};
+        this.opts = opts;
+
+        opts.host = opts.host || "//api.syndy.co.uk";
+        opts.port = opts.port || "";
+        opts.slim = opts.slim || false;
+        opts.path = opts.path || (opts.slim ? "/fixtures/slim" : "/fixtures");
+        if (opts.port) {
+            opts.host = opts.host + ":" + opts.port;
+        }
+    }
+
     /**
      * Loads the fixture data, and returns a jQuery Deferred.
      * This Deferred can be used to attach other event handlers
      * to the Ajax request.
      */
-    this.load = function (params) {
+    FixturesService.prototype.load = function(params) {
+        var self = this;
+        var opts = this.opts;
+
         var s = "";
         if (params) {
             s = ("string" !== typeof params) ? $.param(params) : params;
             s += "&";
         }
         s += "callback=?";
-        var url = host + "/fixtures/slim?" + s;
-        return $.getJSON(url).done(function (data) {
+
+        var url = opts.host + opts.path + "?" + s;
+
+        return $.getJSON(url).then(function (data) {
             if (!data.errors || data.errors.length < 1) {
-                data.rounds = getRoundsFromFixtures(data.fixtures);
+                return handleData(data);
             }
+            return data;
         }).fail(function (response, status, xhr) {
             if (status !== "error") {
                 return;
@@ -60,8 +104,7 @@ function FixturesService(host, port) {
             alert(arr.join("\n"));
         });
     };
-}
 
-return FixturesService;
+    return FixturesService;
 
 });
