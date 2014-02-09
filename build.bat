020faa20b4e316b49e0a0f98d5d40384fe7fc96f
www-syndy-co-uk <dev@syndy.co.uk>
2014-02-09 15:58:57 +0000

    Add r.js excludes for backbone and jqueryui.

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set r_js_shared_opts=-o baseUrl=js name=main paths.jquery=empty: paths.underscore=empty:
+set r_js_shared_opts=-o baseUrl=js name=main paths.jquery=empty: paths.underscore=empty: paths.backbone=empty: paths.jqueryui=empty:
 
 call r.js.cmd %r_js_shared_opts% out=temp.js optimize=none 
 
