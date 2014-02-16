e00111afb90faced6ebfa1d4e125c6f827959e15
www-syndy-co-uk <dev@syndy.co.uk>
2014-02-16 01:02:40 +0000

    Try to separate logic from UI some more.

--- js/syndy/odds/OddsListView.js
+++ js/syndy/odds/OddsListView.js
@@ -18,7 +18,49 @@ define([
         return 0;
     }
 
+    /**
+     * Ties up the team1 and team2 bet options to a single row per bookmaker.
+     */
+    function createBetRows(match) {
+        match.bets.sort(compareByBetBookmaker);
+
+        var curBookie = {};
+
+        var rows = [];
+
+        _.forEach(match.bets, function(bet) {
+            //console.log(betIdx + "," + bet.name + "," + bet.bookmaker);
+            if (bet.handicap && bet.handicap.length > 0 && bet.handicap[0] == "+") {
+                bet.handicap = bet.handicap.substring(1);
+            }
+            if (null != curBookie.bookmaker && bet.bookmaker != curBookie.bookmaker) {
+                rows.push(curBookie);
+                curBookie = {};
+            }
+            if ("Draw" === bet.name) {
+                curBookie.draw = bet;
+            } else if (match.team1 === bet.name) {
+                curBookie.bet1 = bet;
+            } else {
+                curBookie.bet2 = bet;
+            }
+            curBookie.bookmaker = bet.bookmaker;
+        });
+
+        // Add the last one. Why? Because above we only add a bookie if the bookie changes.
+        // For the last bookie this will never happen.
+        rows.push(curBookie);
+
+        return rows;
+    }
+
     function createMatchElement(match, matchTpl) {
+
+        function extendBet(bet) {
+            return _.extend({}, bet, {
+                odds: ("undefined" !== typeof bet.odds) ? Odds.toString(bet.odds) : ""
+            });
+        }
 
         function createBookmakersOddsEl(curBookie) {
             var bet1 = curBookie.bet1 || {};
@@ -28,8 +70,8 @@ define([
             var ob = {
                 // check both bets in case there is only one provided.
                 bookmaker: bet1.bookmaker || bet2.bookmaker,
-                bet1: $.extend({}, bet1, {odds: Odds.toString(bet1.odds) }),
-                bet2: $.extend({}, bet2, {odds: Odds.toString(bet2.odds) }),
+                bet1: extendBet(bet1),
+                bet2: extendBet(bet2),
                 draw: { odds: curBookie.draw ? Odds.toString(curBookie.draw.odds) : "" },
                 team1ColourCssClass: team1ColourCssClass ? ("sprite " + team1ColourCssClass) : "",
                 team2ColourCssClass: team2ColourCssClass ? ("sprite " + team2ColourCssClass) : ""
@@ -52,34 +94,10 @@ define([
 
         var elBets = elMatch.find(".bets tbody");
 
-        match.bets.sort(compareByBetBookmaker);
-
-        var curBookie = {};
-
-        $(match.bets).each(function (betIdx, bet) {
-            //console.log(betIdx + "," + bet.name + "," + bet.bookmaker);
-            if (bet.handicap && bet.handicap.length > 0 && bet.handicap[0] == "+") {
-                bet.handicap = bet.handicap.substring(1);
-            }
-            if (null != curBookie.bookmaker && bet.bookmaker != curBookie.bookmaker) {
-                var elBet = createBookmakersOddsEl(curBookie);
-                elBets.append(elBet);
-                curBookie = {};
-            }
-            if ("Draw" === bet.name) {
-                curBookie.draw = bet;
-            } else if (match.team1 === bet.name) {
-                curBookie.bet1 = bet;
-            } else {
-                curBookie.bet2 = bet;
-            }
-            curBookie.bookmaker = bet.bookmaker;
+        _.forEach(createBetRows(match), function(row) {
+            var elBet = createBookmakersOddsEl(row);
+            elBets.append(elBet);
         });
-
-        // Add the last one. Why? Because above we only add a bookie if the bookie changes.
-        // For the last bookie this will never happen.
-        var elBet = createBookmakersOddsEl(curBookie);
-        elBets.append(elBet);
 
         return elMatch;
     }
@@ -103,10 +121,10 @@ define([
 
             this.$el.html("");
 
-            if (data.errors) {
+            if (data.error) {
                 this.$el.html(data.url + ", " + data.statusText);
             } else {
-                this.$el.append(createMatchElement(data.match, this.matchTpl));
+                this.$el.append(createMatchElement(data, this.matchTpl));
             }
 
             return this;
@@ -114,6 +132,7 @@ define([
     });
 
     OddsListView.createMatchElement = createMatchElement;
+    OddsListView.createBetRows = createBetRows;
 
     return OddsListView;
 
