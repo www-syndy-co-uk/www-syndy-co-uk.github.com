3f430eee0f6a846864e72b7d7be0de2c714c2cce
www-syndy-co-uk <dev@syndy.co.uk>
2014-02-16 01:02:08 +0000

    Better parameter checking.

--- js/syndy/odds/Odds.js
+++ js/syndy/odds/Odds.js
@@ -4,17 +4,23 @@ define(function() {
 
     Odds.toString = function(odds) {
         if (!odds) {
-            return "?";
+            throw new Error("Cannot format " + odds + " as odds.");
         }
         var f = odds.asFraction;
         var d = odds.asDouble;
-        if (f) {
+        if (f && "number" === typeof f.numerator && "number" === typeof f.denominator) {
             return "" + f.numerator + "/" + f.denominator;
         }
-        return d;
+        if ("number" === typeof d) {
+            return "" + d;
+        }
+        throw new Error("Cannot format " + odds + " as odds.");
     };
 
     Odds.strToFloat = function(s) {
+        if (!s || "string" !== typeof s) {
+            throw new Error("Cannot convert " + s + " to float.");
+        }
         var idx = s.indexOf("/");
         if (idx < 0) {
             return parseFloat(s);
@@ -22,6 +28,15 @@ define(function() {
         var num = s.substring(0, idx).trim();
         var denom = s.substring(idx + 1).trim();
         return parseInt(num) / parseInt(denom);
+    };
+
+    Odds.fractional = function(numerator, denominator) {
+        return {
+            asFraction: {
+                numerator: numerator,
+                denominator: denominator
+            }
+        };
     };
 
     return Odds;
